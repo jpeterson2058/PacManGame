b77e0bca47360b651b9b324b716aad0b32bad9b5
Mark Smith <msmith4@gmail.com>
2017-07-10 21:49:33 -0600

    Added left/right movement

--- PacManTest.java
+++ PacManTest.java
@@ -33,9 +33,36 @@ public class PacManTest
 			case 0: instructions();
 			case 1: game.pacMan.direction = game.pacMan.left;
 			break;
-			case 2:game.pacMan.direction = game.pacMan.right;
+			case 2: game.pacMan.direction = game.pacMan.right;
 			break;
-			case 3:game.pacMan.direction = game.pacMan.direction;
+			case 3: 
+                switch (game.pacMan.direction)
+                {
+                    case '>':
+                        if (game.pacMan.posX > 0)
+                        {
+                            game.pacMan.posX--;
+                        }
+                        break;
+                    case '<':
+                        if (game.pacMan.posX < (column - 1))
+                        {
+                            game.pacMan.posX++;
+                        }
+                        break;
+                    case 'v':
+                        if (game.pacMan.posY > 0)
+                        {
+                            game.pacMan.posY--;
+                        }
+                        break;
+                    case '^':
+                        if (game.pacMan.posY < row)
+                        {
+                            game.pacMan.posY++;
+                        }
+                        break;
+                }
 			break;
 			}
 			
@@ -60,6 +87,9 @@ public class PacManTest
 	{
 		
         public char board[][];
+        private int curX = 0;
+        private int curY = 0;
+
         Board(int row, int column)
         		{
         			board = new char [row] [column];
@@ -84,13 +114,21 @@ public class PacManTest
                 
         public void drawBoard(int posX, int posY, char direction)
         {
+            // Replace where PacMan was with a .
+            board[curX][curY] = '.'; 
+
+            // Set PacMan on the board facing the direction
         	board[posX][posY] = direction;
+            
+            // Remember where PacMan is
+            curX = posX;
+            curY = posY;
         	
         	for(int y = 0; y<board.length; y++)
         	{
         		for(int x = 0; x<board.length; x++)
         		{
-        			System.out.print(board[y][x] + " ");
+        			System.out.print(board[x][y] + " ");
         		}
         		System.out.println("");
         	}
@@ -112,7 +150,7 @@ public class PacManTest
         	PacManMove()
         	{
         		this.left = '>';
-        		this.up = 'V';
+        		this.up = 'v';
         		this.right = '<';
         		this.down = '^';
         		
